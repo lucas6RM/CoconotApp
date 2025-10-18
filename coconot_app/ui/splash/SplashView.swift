2b9b52fa720867db6961cefa08b9777b2b42d68e
lucasmercier <lucas-mercier@hotmail.fr>
2025-10-18 11:58:49 +0200

    ok pour pres

--- coconot_app/ui/splash/SplashView.swift
+++ coconot_app/ui/splash/SplashView.swift
@@ -18,16 +18,27 @@ struct SplashView: View {
         
         ZStack{
             Color(.green)
-            VStack{
+            
+            VStack (spacing:0){
+                Spacer()
+                Image("logo2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 400)
+                    
+                
                 Text(String(localized: "SerrIA"))
-                    .font(.largeTitle)
+                    .font(.system(size: 80))
                     .fontWeight(.heavy)
                     .foregroundStyle(.white)
+                Spacer()
             }
             .opacity(isAnimationVisible ? 0 : 1)
             .scaleEffect(isAnimationVisible ? 0.8 : 1)
             .animation(.easeInOut(duration: 1), value: isAnimationVisible)
+            
         }
+        
         .task {
             vm.fetchAllHotHouses()
         }
